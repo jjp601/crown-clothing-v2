d29d275b508381aa572b6de133188671096f4ad8
Jon Powell <jpowell68601@gmail.com>
2022-08-09 12:50:42 -0500

    Setup products data Products Card component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/Home.component";
 import Navbar from "./routes/navbar/Navbar.component";
 import SignIn from "./routes/signin/SignIn.component";
+import Shop from "./routes/shop/Shop.component";
 
 const App = () => {
 
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navbar />}>
         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
         <Route path='signin' element={<SignIn />} />
       </Route>
     </Routes>
