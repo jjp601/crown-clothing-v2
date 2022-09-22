40df041e988e68ddf64a21b24fb876dc385cb4ba
Jon Powell <jpowell68601@gmail.com>
2022-09-22 12:24:39 -0500

    Update application styles for mobile

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from 'react-redux';
 import { Routes, Route } from "react-router-dom";
 
-import Home from "./routes/home/Home.component";
-import Navbar from "./routes/navbar/Navbar.component";
-import SignIn from "./routes/signin/SignIn.component";
-import Shop from "./routes/shop/Shop.component";
-import Checkout from "./routes/checkout/Checkout.component";
+import Spinner from "./components/Spinner/Spinner.component";
 import { checkUserSession } from "./store/user/user.action";
+
+const Home = lazy(() => import('./routes/home/Home.component'));
+const SignIn = lazy(() => import('./routes/signin/SignIn.component'));
+const Navbar = lazy(() => import('./routes/navbar/Navbar.component'));
+const Shop = lazy(() => import('./routes/shop/Shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/Checkout.component'));
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,14 +19,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navbar />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='signin' element={<SignIn />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index element={<Home />} />
+            <Route path='shop/*' element={<Shop />} />
+            <Route path='signin' element={<SignIn />} />
+            <Route path='checkout' element={<Checkout />} />
+          </Route>
+        </Routes>
+    </Suspense>
   );
 }
 
--- src/components/CategoryPreview/CategoryPreview.component.jsx
+++ src/components/CategoryPreview/CategoryPreview.component.jsx
@@ -1,24 +1,22 @@
-import { Link } from 'react-router-dom';
-
 import ProductCard from '../ProductCard/ProductCard.component';
 
-import './CategoryPreview.styles.scss';
+import { CategoryPreviewContainer, Title, Preview } from './CategoryPreview.styles';
 
 const CategoryPreview = ({ title, products }) => {
     return (
-        <div className='category-preview-container'>
+        <CategoryPreviewContainer>
             <h2>
-                <Link className='title' to={title}>{title.toUpperCase()}</Link>
+                <Title to={title}>{title.toUpperCase()}</Title>
             </h2>
-            <div className="preview">
+            <Preview>
                 {
                     products.filter((_, idx) => idx < 4)
                     .map((product) => (
                         <ProductCard key={product.id} product={product} />
                     ))
                 }
-            </div>
-        </div>
+            </Preview>
+        </CategoryPreviewContainer>
     );
 };
 
