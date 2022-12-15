b041c1d7b2b8c9172c127d23186343a63e7414cf
Jon Powell <jpowell68601@gmail.com>
2022-12-15 13:28:15 -0600

    Update Checkout Item to jsx component

--- src/components/CheckoutItem/CheckoutItem.component.jsx
+++ src/components/CheckoutItem/CheckoutItem.component.jsx
@@ -3,7 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectCartItems } from '../../store/cart/cart.selector';
 import { addItemToCart, removeItemFromCart, clearItemFromCart } from '../../store/cart/cart.action';
 
-import './CheckoutItem.styles.scss';
+
+import { CheckoutItemContainer, ImageContainer, BaseSpan, Quantity, Arrow, Value, RemoveButton } from './CheckoutItem.styles';
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
@@ -15,19 +16,19 @@ const CheckoutItem = ({ cartItem }) => {
     const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
 
     return (
-        <div className='checkout-item-container'>
-            <div className="image-container">
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt={`${name}`} />
-            </div>
-            <span className='name'>{name}</span>
-            <span className='quantity'>
-                <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
-                <span className="value">{quantity}</span>
-                <div className='arrow' onClick={addItemHandler}>&#10095;</div>
-            </span>
-            <span className='price'>{price}</span>
-            <div className="remove-button" onClick={clearItemHandler}>&#10005;</div>
-        </div>
+            </ImageContainer>
+            <BaseSpan>{name}</BaseSpan>
+            <Quantity>
+                <Arrow onClick={removeItemHandler}>&#10094;</Arrow>
+                <Value>{quantity}</Value>
+                <Arrow onClick={addItemHandler}>&#10095;</Arrow>
+            </Quantity>
+            <BaseSpan>{price}</BaseSpan>
+            <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
     );
 };
 
